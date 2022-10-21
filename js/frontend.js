e41631a88db21e5721039ae20051c242e86e7747
Lucia Arenas <sanera.lu@gmail.com>
2022-10-21 19:57:04 -0300

    principio-js

---
+++ js/frontend.js
@@ -0,0 +1,7 @@
+let valorTicket = 200;
+let estudiante = valorTicket * 0.2;
+let trainee = valorTicket * 0.5;
+let junior = valorTicket * 0.85;
+let precioFinal;
+categoria = document.getElementById("categorias");
+

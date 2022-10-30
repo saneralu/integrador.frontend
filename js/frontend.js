5e35493687572d7c5ed11d355c9113382cb189ad
Lucia Arenas <sanera.lu@gmail.com>
2022-10-30 15:42:46 -0300

    prueba-final

--- js/frontend.js
+++ js/frontend.js
@@ -1,7 +1,30 @@
+const categoria = document.getElementById("categorias");
+const cantidad = document.getElementById("cantidad");
 let valorTicket = 200;
-let estudiante = valorTicket * 0.2;
-let trainee = valorTicket * 0.5;
-let junior = valorTicket * 0.85;
-let precioFinal;
-categoria = document.getElementById("categorias");
+let valorTotal;
+const calcular = document.getElementById("btnResumen");
+const reset = document.getElementById("btnReset");
 
+function precio() {
+    if (categoria.value == "estudiante") {
+        valorTotal = cantidad.value * (valorTicket * 0.2)
+    } else if (categoria.value == "trainee") {
+        valorTotal = cantidad.value * (valorTicket * 0.5)
+    } else if (categoria.value == "junior") {
+        valorTotal = cantidad.value * (valorTicket * 0.85)
+    };
+    document.getElementById("precio").innerHTML = `Total a Pagar: $${valorTotal}`;
+}
+
+function borrar() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("correo").value = "";
+    document.getElementById("cantidad").value = "";
+    categoria.value = "";
+    document.getElementById("precio").innerHTML = "Total a Pagar: $";
+
+}
+
+calcular.onclick = precio;
+reset.onclick = borrar;
